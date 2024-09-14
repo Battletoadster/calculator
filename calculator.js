918e18c22e94b576b5f6abe2dca4417b315468fb
Battletoadster <72815818+Battletoadster@users.noreply.github.com>
2024-09-14 11:50:46 +0200

    Add operation functionality

--- calculator.js
+++ calculator.js
@@ -17,3 +17,22 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1/num2;
 }
+
+function operate(fstOperand, sndOperand, operator){
+    switch (operator){
+        case "+":
+            return add(fstOperand, sndOperand);
+
+        case "-":
+            return subtract(fstOperand, sndOperand)
+
+        case "*":
+            return multiply(fstOperand, sndOperand);
+        
+        case "/":
+            return divide(fstOperand, sndOperand);
+
+        default:
+            return "INVALID INPUT";
+    }
+}
